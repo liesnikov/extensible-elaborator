fe552c0348079a6545cf26c49f22200dd4a83374
Bohdan <b.liesnikov+git@gmail.com>
2022-06-30 10:51:35 +0200

    simplify build by calling cabal2nix directily from nix

--- extended/shell.nix
+++ extended/shell.nix
@@ -1,9 +1,5 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
-
+{compiler ? "ghc922"}:
 let
-
-  pi-forall = import ./default.nix {nixpkgs=nixpkgs; compiler=compiler;};
-
+  pi-forall = import ./default.nix {inherit compiler;};
 in
-
   pi-forall.env
