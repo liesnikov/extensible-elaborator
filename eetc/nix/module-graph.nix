7c2c91ebd78f0c0847858fb9275fa4e0b0542a2c
Bohdan <b.liesnikov+git@gmail.com>
2022-11-21 18:05:24 +0100

    add dysfunctional module-graph builder

---
+++ eetc/nix/module-graph.nix
@@ -0,0 +1,24 @@
+let
+  defpkgs = builtins.fetchTarball {
+    # Descriptive name to make the store path easier to identify
+    name = "nixos-22.05.1271.babb041b716";
+    url = "https://releases.nixos.org/nixos/22.05-small/nixos-22.05.1271.babb041b716/nixexprs.tar.xz";
+    # Hash obtained using `nix-prefetch-url --unpack <url>`
+    sha256 = "0g8vwni83zn6kgkczrm5vwmyhl473rrs9d4k4hn5gfbgfsyv7ls8";
+  };
+  plugin-overlay-git = builtins.fetchGit
+   { url = https://github.com/mpickering/haskell-nix-plugin.git;}  ;
+  plugin-overlay = import "${plugin-overlay-git}/overlay.nix";
+
+  config = compiler: import ./nix/compilerconfig.nix compiler;
+
+in
+{ compiler ? "ghc922" }:
+let
+  nixpkgs = import defpkgs {compiler = (config compiler);
+                            overlays = [plugin-overlay];};
+  hl = nixpkgs.haskell.lib;
+  hp = nixpkgs.haskellPackages;
+  myPackage = import ./. {compiler = compiler; nixpkgs = nixpkgs;};
+in
+  (hp.withPlugin(plugs: ps: hl.addPlugin plugs.dump-core myPackage))
