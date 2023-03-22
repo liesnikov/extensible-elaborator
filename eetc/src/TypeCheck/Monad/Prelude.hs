243f6c32675d6804e70e8de52210f92eddc7b054
Bohdan <b.liesnikov+git@gmail.com>
2023-03-22 11:19:01 +0100

    remove modules that aren't used from Monad.Prelude

--- eetc/src/TypeCheck/Monad/Prelude.hs
+++ eetc/src/TypeCheck/Monad/Prelude.hs
@@ -1,8 +1,6 @@
 module TypeCheck.Monad.Prelude
   ( module TypeCheck.Monad.Prelude
   , module Data.Kind
---  , module Map
---  , module Set
   , module Control.Monad.IO.Class
   , module Control.Monad.Trans
   , module Control.Monad.Trans.Control
@@ -12,43 +10,23 @@ module TypeCheck.Monad.Prelude
 where
 
 import           Data.Kind (Type)
-import qualified Data.Map.Strict as Map
-import qualified Data.Set as Set
 
-import           Control.Monad (join, MonadPlus(..))
-import           Control.Monad.Except ( MonadError(..)
-                                      , ExceptT
-                                      , runExceptT )
 import           Control.Monad.IO.Class ( MonadIO(..) )
-import           Control.Monad.Reader ( ReaderT(runReaderT)
-                                      , ask
-                                      , local )
-import           Control.Monad.State ( StateT(runStateT)
-                                      , put
-                                      , modify
-                                      , get )
 import           Control.Monad.Trans ( MonadTrans(..)
                                       , lift )
 import           Control.Monad.Trans.Control ( MonadTransControl(..)
                                              , liftThrough )
 
-import qualified Unbound.Generics.LocallyNameless as Unbound
 
-
-import           PrettyPrint           ( D(..)
-                                       , Disp(..)
-                                       , Disp1
-                                       , render
-                                       )
-import qualified Syntax.Internal as I
-import           Syntax.Internal       ( Meta(..)
-                                       , MetaTag(..)
-                                       , MetaVarId )
+import           PrettyPrint
 import           Syntax.SourceLocation (SourceLocation)
 import           TypeCheck.Constraints ( ConstraintF
                                        , BasicConstraintsF
                                        , (:<:)
                                        , inject )
+-- FIXME
+-- I don't understand the import rules here
+-- this setup seems to work but deleting either import leads to compilation errors
 import qualified TypeCheck.State as State
 import           TypeCheck.State       ( Env(..)
                                        , Err(..)
