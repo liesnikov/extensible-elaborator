94438e99be9efded31834c6c6303f496ecfe5bc6
Bohdan <b.liesnikov+git@gmail.com>
2023-03-22 11:29:51 +0100

    figure out imports

--- eetc/src/TypeCheck/Monad/Prelude.hs
+++ eetc/src/TypeCheck/Monad/Prelude.hs
@@ -5,33 +5,22 @@ module TypeCheck.Monad.Prelude
   , module Control.Monad.Trans
   , module Control.Monad.Trans.Control
   , module TypeCheck.Constraints
+  , module TypeCheck.State
   , module PrettyPrint
-  , module State)
-where
-
-import           Data.Kind (Type)
+  , module Syntax.SourceLocation
+  ) where
 
 import           Control.Monad.IO.Class ( MonadIO(..) )
-import           Control.Monad.Trans ( MonadTrans(..)
-                                      , lift )
-import           Control.Monad.Trans.Control ( MonadTransControl(..)
-                                             , liftThrough )
+import           Control.Monad.Trans ( MonadTrans(..), lift )
+import           Control.Monad.Trans.Control ( MonadTransControl(..), liftThrough )
+import           Data.Kind (Type)
 
-
-import           PrettyPrint
+import           PrettyPrint ( D(..), Disp(..), Disp1, render)
 import           Syntax.SourceLocation (SourceLocation)
-import           TypeCheck.Constraints ( ConstraintF
-                                       , BasicConstraintsF
-                                       , (:<:)
-                                       , inject )
--- FIXME
--- I don't understand the import rules here
--- this setup seems to work but deleting either import leads to compilation errors
-import qualified TypeCheck.State as State
-import           TypeCheck.State       ( Env(..)
-                                       , Err(..)
-                                       , NameMap
-                                       , vars
-                                       )
+
+import           TypeCheck.Constraints ( ConstraintF, BasicConstraintsF, (:<:), inject )
+import           TypeCheck.State ( Env(..) , Err(..), NameMap, vars)
+
+import qualified TypeCheck.State as State (TcState(..))
 
 type TcState m c = State.TcState (m ()) c
