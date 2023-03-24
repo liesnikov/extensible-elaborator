bf0b966c7af0159a64057d4196ed3ec7e4e0531b
Bohdan <b.liesnikov+git@gmail.com>
2023-03-24 16:54:20 +0100

    initial implementation of a compilation of plugins

--- eetc/src/TypeCheck/Solver/Allsolver.hs
+++ eetc/src/TypeCheck/Solver/Allsolver.hs
@@ -6,11 +6,51 @@ import TypeCheck.Constraints
 import TypeCheck.Monad.Typeclasses
 import TypeCheck.Solver.Base
 
+type Allsolver c = [Plugin c]
 
-type Allsolver c = [BlindPlugin c]
+-- given a linearly ordered allsolver find a place for a new plugin p
+-- such that p is before all solvers specified by the pre field
+-- and after all solvers specified by the suc field of the plugin p
+insert :: Plugin c -> Allsolver c -> Allsolver c
+insert p s = reverse $ insert' p s []
+  where
+    -- acc stores reversed linear order of the already traversed plugins in the allsolver
+    -- if we reach the end of the list insert p in the last place
+    -- so head element, since the list is reversed
+    insert' :: Plugin c -> Allsolver c -> Allsolver c -> Allsolver c
+    insert' p [] acc = p : acc
+    insert' p (h : t) acc =
+      let
+        psym = symbol p
+        hsym = symbol h
+      in
+      -- ensure there's no inconsistency
+        if (psym `elem` pre h || hsym `elem` suc p) &&
+           (psym `elem` suc h || hsym `elem` pre p)
+        then error $ "inconsistent order given for symbols" ++
+             (show p) ++ " and " ++ (show h)
+        else -- does p have to be before h?
+          if (psym `elem` pre h || hsym `elem` suc p)
+          then acc ++ reverse t ++ [h] ++ [p]
+          else -- does p have to be after h?
+            if (psym `elem` suc h || hsym `elem` pre p)
+            then acc ++ [p] ++ [h] ++ reverse t
+            else -- this means head symbol isn't related
+                 -- to the first symbol in the linear order
+                 -- so we keep traversing
+                 insert' p t (acc ++ [h])
 
-compile :: [BlindPlugin c] -> Allsolver c
-compile = undefined
+
+-- use pre and suc fields of the plugin that define a preorder to build a linear sequence
+compile :: [Plugin c] -> Allsolver c
+compile [] = []
+compile (h : t) =
+  -- recursive call, linearize all other plugins, only leaving h to be inserted
+  let rest = compile t
+      -- insert h into the linearized rest
+      -- if h is independent of rest, it will be inserted at the end
+      -- otherwise it will be inserted at the right place
+  in insert h rest
 
 solve :: (MonadElab c m) => Allsolver c -> (ConstraintF c) -> m (Maybe PluginId)
 solve [] cs = return Nothing
