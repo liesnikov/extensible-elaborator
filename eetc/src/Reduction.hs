54edba0064a5fc70d87be2e757ad0fb06967e238
Bohdan <b.liesnikov+git@gmail.com>
2023-10-24 15:06:12 +0200

    add reduction with blockers

--- eetc/src/Reduction.hs
+++ eetc/src/Reduction.hs
@@ -1,82 +1,120 @@
 -- | reduction in the presence of metavariables
 
-module Reduction where
+module Reduction (whnf) where
 
-import Control.Monad.Except (MonadError(..), zipWithM)
+import Control.Monad.Except (MonadError, zipWithM, catchError)
+--only available from mtl 2.3
+--import Control.Monad.Error.Class (tryError)
 
 import Syntax.Internal
-import TypeCheck.Environment ( D(DS, DD))
+import PrettyPrint ( D(DS, DD) )
 import qualified TypeCheck.Environment as Env (err)
 import TypeCheck.State (Err)
-import TypeCheck.StateActions as SA
+import TypeCheck.StateActions as SA ( lookupAnyDef
+                                    , lookupMetaVarSolution)
+import TypeCheck.Blockers ( Blocker, blockOnMeta )
 import TypeCheck.Monad (MonadTcReader(..), MonadTcReaderEnv(..))
 import qualified Unbound.Generics.LocallyNameless as Unbound
 
-reduce :: (MonadTcReader m, MonadTcReaderEnv m,
-           MonadError Err m, Unbound.Fresh m) => Term -> m Term
-reduce (Var x) = do
-  maybeDef <- SA.lookupDef x
+whnf :: (MonadTcReader m, MonadTcReaderEnv m,
+         MonadError Err m, Unbound.Fresh m) => Term -> m (Term, Maybe Blocker)
+whnf (Var x) = do
+  maybeDef <- SA.lookupAnyDef x
   case maybeDef of
-    (Just d) -> reduce d
-    _ -> do
-          maybeRecDef <- SA.lookupRecDef x
-          case maybeRecDef of
-            (Just d) -> reduce d
-            _ -> return (Var x)
+    (Just d) -> whnf d
+    _ -> return (Var x, Nothing)
 
-reduce (App t1 t2) = do
-  nf <- reduce t1
-  case nf of
-    (Lam ep  bnd) -> do
-      reduce (Unbound.instantiate bnd [unArg t2] )
-    _ -> do
-      return (App nf t2)
-
-reduce (If t1 t2 t3) = do
-  nf <- reduce t1
-  case nf of
-    (LitBool bo) -> if bo then reduce t2 else reduce t3
-    _ -> return (If nf t2 t3)
-
-reduce (LetPair a bnd) = do
-  nf <- reduce a
-  case nf of
-    Prod b1 c -> do
-      reduce (Unbound.instantiate bnd [b1, c])
-    _ -> return (LetPair nf bnd)
+whnf (App t1 t2) = do
+  (nf, mblock) <- whnf t1
+  case mblock of
+    Just b -> return (App nf t2, Just b)
+    Nothing -> case nf of
+      (Lam ep bnd) -> return (Unbound.instantiate bnd [unArg t2], Nothing)
+      _ -> Env.err [ DS "reduction of an application head didn't block and returned a non-lambda"
+                   , DD nf
+                   ]
 
 -- ignore/remove type annotations and source positions when normalizing
-reduce (Ann tm _) = reduce tm
-reduce (Pos _ tm) = reduce tm
+whnf (Ann tm _) = whnf tm
+whnf (Pos _ tm) = whnf tm
 
-reduce (Let rhs bnd)  = do
-  reduce (Unbound.instantiate bnd [rhs])
-reduce (Subst tm pf) = do
-  pf' <- reduce pf
-  case pf' of
-    Refl -> reduce tm
-    _ -> return (Subst tm pf')
-reduce (Case scrut mtchs) = do
-  nf <- reduce scrut
-  case nf of
-    (DCon d args) -> f mtchs where
-      f (Match bnd : alts) = (do
+whnf (Let rhs bnd)  = do
+  whnf (Unbound.instantiate bnd [rhs])
+
+whnf (If t1 t2 t3) = do
+  (nf, mblock) <- whnf t1
+  case mblock of
+    Just b -> return (If nf t2 t3, Just b)
+    Nothing -> case nf of
+        (LitBool True) -> whnf t2
+        (LitBool False) -> whnf t3
+        _ -> Env.err [ DS "reduction of an if condition didn't block and returned a non-boolean"
+                     , DD nf
+                     ]
+
+
+whnf (LetPair a bnd) = do
+  (nf, mblock) <- whnf a
+  case mblock of
+    Just b -> return (LetPair nf bnd, Just b)
+    Nothing -> case nf of
+      (Prod b1 c) -> whnf (Unbound.instantiate bnd [b1, c])
+      _ -> Env.err [ DS "reduction of a pair didn't block and returned a non-pair"
+                   , DD nf
+                   ]
+
+
+whnf (Subst tm pf) = do
+  (tm', mb) <- whnf tm
+  case mb of
+    Just b -> return (Subst tm' pf, Just b)
+    Nothing -> do
+      case tm' of
+        Refl -> whnf pf
+        _ -> Env.err [ DS "reduction of an equality proof in a subst didn't block and returned a non-refl"
+                     , DD tm'
+                     ]
+
+
+whnf (Contra t) = do
+  (nf, mblock) <- whnf t
+  case mblock of
+    Just b -> return (Contra nf, Just b)
+    Nothing -> return (Contra nf, Nothing)
+
+whnf (Case scrut mtchs) = do
+  (nf, mblock) <- whnf scrut
+  case mblock of
+    Just b -> return (Case nf mtchs, Just b)
+    Nothing -> case nf of
+      (DCon d args) -> f mtchs where
+        f :: (MonadTcReader m, MonadTcReaderEnv m, MonadError Err m,
+              Unbound.Fresh m) =>
+              [Match] -> m (Term, Maybe Blocker)
+        f (Match bnd : alts) = do
           (pat, br) <- Unbound.unbind bnd
-          ss <- patternMatches (Arg Rel nf) pat
-          reduce (Unbound.substs ss br))
-            `catchError` \ _ -> f alts
-      f [] = Env.err $ [DS "Internal error: couldn't find a matching",
-                        DS "branch for", DD nf, DS "in"] ++ map DD mtchs
-    _ -> return (Case nf mtchs)
+          mss <- tryError $ patternMatches (Arg Rel nf) pat
+          case mss of
+            Left _ -> f alts
+            Right (Left b) -> return (Case nf mtchs, Just b)
+            Right (Right ss) -> whnf (Unbound.substs ss br)
+        f [] = Env.err $ [DS "Internal error: couldn't find a matching",
+                          DS "branch for", DD nf, DS "in"] ++ map DD mtchs
+      _ -> return (Case nf mtchs, Nothing)
 
 -- metavariables are substituted
-reduce tm@(MetaVar _) = do
+whnf tm@(MetaVar _) = do
   msol <- SA.lookupMetaVarSolution tm
-  maybe (return tm) (reduce) msol
+  case msol of
+    (Just sol) -> whnf sol
+    Nothing ->
+     maybe (Env.err [DS "Internal error: couldn't block on a meta", DD tm])
+           (\b -> return (tm, Just b))
+           (blockOnMeta tm)
 
--- all other terms are already in REDUCE
+-- all other terms are already in WHNF
 -- don't do anything special for them
-reduce tm = return tm
+whnf tm = return (tm, Nothing)
 
 
 -- | Determine whether the pattern matches the argument
@@ -84,14 +122,28 @@ reduce tm = return tm
 -- otherwise throws an error
 patternMatches :: (MonadTcReader m, MonadTcReaderEnv m,
                    MonadError Err m, Unbound.Fresh m)
-               => Arg -> Pattern -> m [(TName, Term)]
-patternMatches (Arg _ t) (PatVar x) = return [(x, t)]
-patternMatches (Arg Rel t) pat = do
-  nf <- reduce t
-  case (nf, pat) of
-    (DCon d [], PatCon d' pats)   | d == d' -> return []
-    (DCon d args, PatCon d' pats) | d == d' ->
-       concat <$> zipWithM patternMatches args (map fst pats)
-    _ -> Env.err [DS "arg", DD nf, DS "doesn't match pattern", DD pat]
+               => Arg -> Pattern -> m (Either Blocker [(TName, Term)])
+patternMatches (Arg _ t) (PatVar x) = return . Right $ [(x, t)]
+patternMatches (Arg Rel t) pat@(PatCon dp pats) = do
+  (nf, mb) <- whnf t
+  case mb of
+    Nothing ->
+      case nf of
+        (DCon d [])   | d == dp -> return . Right $ []
+        (DCon d args) | d == dp -> do
+           v <- sequence <$> zipWithM patternMatches args (map fst pats)
+           return $ either (Left) (Right . concat) v
+        _ -> Env.err [DS "arg", DD nf, DS "doesn't match pattern", DD pat]
+    Just b ->
+      case nf of
+        (DCon d [])   | d == dp -> return . Right $ []
+        (DCon d args) | d == dp -> do
+           v <- sequence <$> zipWithM patternMatches args (map fst pats)
+           return $ either (Left) (Right . concat) v
+        _ -> return $ Left b
 patternMatches (Arg Irr _) pat = do
   Env.err [DS "Cannot match against irrelevant args"]
+
+
+tryError :: MonadError e m => m a -> m (Either e a)
+tryError action = (Right <$> action) `catchError` (pure . Left)
