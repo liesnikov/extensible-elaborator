cd7ee86f0a1326b48ff33855e0a2eac52d43062b
Bohdan <b.liesnikov+git@gmail.com>
2023-10-25 21:38:36 +0200

    compiling occurs-checker

--- eetc/src/TypeCheck/OccursCheck.hs
+++ eetc/src/TypeCheck/OccursCheck.hs
@@ -11,9 +11,8 @@ import qualified Syntax.InternalSyntax as I
       MetaVarId,
       TName )
 
-import           Reduction (whnf)
 import           PrettyPrint (D(..))
-
+import           Reduction (whnf)
 import qualified TypeCheck.Environment as Env
 import           TypeCheck.Monad.Typeclasses (MonadSolver)
 
@@ -22,181 +21,208 @@ import qualified Unbound.Generics.LocallyNameless as Unbound
 data PruningPosition =
     Flexible
   | Rigid
-  | StrongRigid
   deriving (Eq, Show)
 
-data OccursData = OccursData {
-    globals :: [I.TName]
-  , locals ::  [I.TName]
-  , current :: I.MetaVarId
-  , position :: PruningPosition
-  } deriving (Eq, Show)
+instance Semigroup PruningPosition where
+  -- anything added to Flexible is Flexible, Strong Rigid and Rigid is Rigid
+  Flexible <> p = p
+  p <> Flexible = p
+  Rigid <> Rigid = Rigid
 
--- | entry function for occurs check and pruning
-occursCheck :: (MonadSolver c m) =>
-               I.MetaVarId
-            -> [I.TName] -- ^ list of variables allowed in this term
-            -> I.Term -- ^ the term we're pruning
-            -> m I.Term
--- pattern-match on the term and defer to functions as described by abel & pientka
-occursCheck mid tel I.Type = return I.Type
-occursCheck mid tel (I.Var x) = I.Var <$> occursCheckVar mid tel x
-occursCheck mid tel (I.Lam h b) = do
-  -- ^ extend the context of allowed vars with a new binding
-  (x, body) <- Unbound.unbind b
-  body' <- occursCheck mid (x : tel) body
-  return $ I.Lam h (Unbound.bind x body')
-occursCheck mid tel (I.App h arg) = do
-  -- ^ occurs check on the head and the argument
-  h' <- occursCheck mid tel h
-  arg' <- occursCheckArg mid tel arg
-  return $ I.App h' arg'
-occursCheck mid tel (I.Pi e h b) = do
-  -- ^ extend the context of allowed vars with a new binding
-  (x, body) <- Unbound.unbind b
-  body' <- occursCheck mid (x : tel) body
-  return $ I.Pi e h (Unbound.bind x body')
+data OccursData where
+  OccursData :: {globals :: [I.TName],
+                 locals :: [I.TName],
+                 current :: I.MetaVarId,
+                 position :: PruningPosition}
+             -> OccursData
+  deriving (Eq, Show)
 
-occursCheck mid tel (I.Ann t ty) = do
-  t' <- occursCheck mid tel t
-  ty' <- occursCheck mid tel ty
-  return $ I.Ann t' ty'
-occursCheck mid tel (I.Pos s t) = do
-  t' <- occursCheck mid tel t
-  return $ I.Pos s t'
-occursCheck mid tel I.TrustMe = return I.TrustMe
-occursCheck mid tel I.PrintMe = return I.PrintMe
-occursCheck mid tel (I.Let t b) = do
-  (x, body) <- Unbound.unbind b
-  body' <- occursCheck mid (x : tel) body
-  t' <- occursCheck mid tel t
-  return $ I.Let t' (Unbound.bind x body')
+extendLocal :: OccursData -> I.TName -> OccursData
+extendLocal (OccursData g l c p) x = OccursData g (x : l) c p
 
--- units
-occursCheck mid tel I.TyUnit = return I.TyUnit
-occursCheck mid tel I.LitUnit = return I.LitUnit
+extendLocals :: OccursData -> [I.TName] -> OccursData
+extendLocals (OccursData g l c p) xs = OccursData g (xs ++ l) c p
 
--- bools
-occursCheck mid tel I.TyBool = return I.TyBool
-occursCheck mid tel (I.LitBool b) = return $ I.LitBool b
-occursCheck mid tel (I.If b t e) = do
-  b' <- occursCheck mid tel b
-  t' <- occursCheck mid tel t
-  e' <- occursCheck mid tel e
-  return $ I.If b' t' e'
+switchPosition :: OccursData -> PruningPosition -> OccursData
+switchPosition (OccursData g l c _) = OccursData g l c
 
--- sigma
-occursCheck mid tel (I.Sigma a body) = do
-  a' <- occursCheck mid tel a
-  (x, body') <- Unbound.unbind body
-  body'' <- occursCheck mid (x : tel) body'
-  return $ I.Sigma a' (Unbound.bind x body'')
+switchToFlexible :: OccursData -> OccursData
+switchToFlexible od = switchPosition od Flexible
 
-occursCheck mid tel (I.Prod a b) = do
-  a' <- occursCheck mid tel a
-  b' <- occursCheck mid tel b
-  return $ I.Prod a' b'
+class Occurs a where
+  occurs :: (MonadSolver c m) =>OccursData -> a -> m a
 
-occursCheck mid tel (I.LetPair a b) = do
-  a' <- occursCheck mid tel a
-  ((x, y), body) <- Unbound.unbind b
-  body' <- occursCheck mid (x : y : tel) body
-  return $ I.LetPair a' (Unbound.bind (x, y) body')
+instance Occurs a => Occurs [a] where
+  occurs env = mapM (occurs env)
 
--- equality
-occursCheck mid tel (I.TyEq a b) = do
-  a' <- occursCheck mid tel a
-  b' <- occursCheck mid tel b
-  return $ I.TyEq a' b'
-occursCheck mid tel I.Refl = return I.Refl
-occursCheck mid tel (I.Subst t e) = do
-  t' <- occursCheck mid tel t
-  e' <- occursCheck mid tel e
-  return $ I.Subst t' e'
-occursCheck mid tel (I.Contra t) = do
-  t' <- occursCheck mid tel t
-  return $ I.Contra t'
+instance Occurs I.Term where
+  occurs env t = do
+    (wt, mb) <- whnf t
+    case mb of
+      Nothing -> case wt of
+        I.Type -> return I.Type
+        (I.Var x) -> I.Var <$> occurs env x
+        (I.Lam h b) -> do
+          -- ^ extend the context of allowed vars with a new binding
+          (x, body) <- Unbound.unbind b
+          let env' = extendLocal env x
+          body' <- occurs env' body
+          return $ I.Lam h (Unbound.bind x body')
+--      (I.App h arg) -> _
+--      (I.Ann t ty) -> _
+--      (I.Pos s t) -> _
+        (I.Pi e h b) -> do
+            -- ^ extend the context of allowed vars with a new binding
+          (x, body) <- Unbound.unbind b
+          let env' = extendLocal env x
+          body' <- occurs env' body
+          return $ I.Pi e h (Unbound.bind x body')
 
--- type constructors
-occursCheck mid tel (I.TCon n args) = do
-  args' <- occursCheckArgs mid tel args
-  return $ I.TCon n args'
--- data constructors
-occursCheck mid tel (I.DCon n args) = do
-  args' <- occursCheckArgs mid tel args
-  return $ I.DCon n args'
--- case
-occursCheck mid tel (I.Case t m) = do
-  t' <- occursCheck mid tel t
-  m' <- occursCheckMatches mid tel m
-  return $ I.Case t' m'
+        I.TrustMe -> return I.TrustMe
+        I.PrintMe -> return I.PrintMe
+--      (I.Let t b) -> _
 
---meta
-occursCheck mid tel (I.MetaVar mclos) = I.MetaVar <$> occursCheckMeta mid tel mclos
+        I.TyUnit -> return I.TyUnit
+        I.LitUnit -> return I.LitUnit
 
-occursCheckMatches :: (MonadSolver c m)
-                   => I.MetaVarId
-                   -> [I.TName]
-                   -> [I.Match]
-                   -> m [I.Match]
-occursCheckMatches mid tel = mapM (occursCheckMatch mid tel)
+        I.TyBool -> return I.TyBool
+        (I.LitBool b) -> return $ I.LitBool b
+--      (I.If b t e) -> _
 
-occursCheckMatch :: (MonadSolver c m)
-                 => I.MetaVarId
-                 -> [I.TName]
-                 -> I.Match
-                 -> m I.Match
-occursCheckMatch mid tel (I.Match body) = do
-  (pat, body') <- Unbound.unbind body
-  body'' <- occursCheck mid (pat2Vars pat ++ tel) body'
-  return $ I.Match (Unbound.bind pat body'')
+        (I.Sigma a body) -> do
+          a' <- occurs env a
+          (x, body') <- Unbound.unbind body
+          let env' = extendLocal env x
+          body'' <- occurs env' body'
+          return $ I.Sigma a' (Unbound.bind x body'')
+
+        (I.Prod a b) -> do
+          a' <- occurs env a
+          b' <- occurs env b
+          return $ I.Prod a' b'
+--     (I.LetPair a b) -> _
+
+        (I.TyEq a b) -> do
+          a' <- occurs env a
+          b' <- occurs env b
+          return $ I.TyEq a' b'
+        I.Refl -> return I.Refl
+--      (I.Subst t e) -> _
+--      (I.Contra t) -> _
+
+        (I.TCon n args) -> do
+          args' <- occurs env args
+          return $ I.TCon n args'
+        (I.DCon n args) -> do
+          args' <- occurs env args
+          return $ I.DCon n args'
+--      (I.Case t alts) -> _
+
+        (I.MetaVar mc) -> I.MetaVar <$> occurs env mc
+        _ -> Env.err [ DS "whnf returned something that doesn't have constructor as a head"
+                     , DD wt]
+      Just mb ->
+        -- we're in a flexible position, so no pruning, only checking
+        let env' = switchToFlexible env
+        in case wt of
+--        I.Type -> _
+          (I.Var x) -> I.Var <$> occurs env' x
+--        (I.Lam h b) -> _
+          (I.App h arg) -> do
+            h' <- occurs env' h
+            arg' <- occurs env' arg
+            return $ I.App h arg'
+
+          (I.Ann t ty) -> do
+            t' <- occurs env' t
+            ty' <- occurs env' ty
+            return $ I.Ann t' ty'
+          (I.Pos s t) -> do
+            t' <- occurs env' t
+            return $ I.Pos s t'
+
+--        (I.Pi e h b) -> _
+
+--        I.TrustMe -> _
+--        I.PrintMe -> _
+          (I.Let t b) -> do
+            t' <- occurs env' t
+            (x, body) <- Unbound.unbind b
+            let env'' = extendLocal env' x
+            body' <- occurs env'' body
+            return $ I.Let t' (Unbound.bind x body')
+
+---       I.TyUnit -> _
+--        I.LitUnit -> _
+
+--          I.TyBool -> _
+--          (I.LitBool b) -> _
+          (I.If b t e) -> do
+            b' <- occurs env' b
+            t' <- occurs env' t
+            e' <- occurs env' e
+            return $ I.If b' t' e'
+
+--        (I.Sigma a body) -> _
+--        (I.Prod a b) -> _
+          (I.LetPair a b) -> do
+            a' <- occurs env' a
+            ((x, y), body) <- Unbound.unbind b
+            let env'' = extendLocals env' [x,y]
+            body' <- occurs env'' body
+            return $ I.LetPair a' (Unbound.bind (x, y) body')
+--          (I.TyEq a b) -> _
+          I.Refl -> return I.Refl
+          (I.Subst t e) -> do
+            t' <- occurs env' t
+            e' <- occurs env' e
+            return $ I.Subst t' e'
+          (I.Contra t) -> do
+            t' <- occurs env' t
+            return $ I.Contra t'
+--        (I.TCon n args) -> _
+--        (I.DCon n args) -> _
+          (I.Case t alts) -> do
+            t' <- occurs env' t
+            alts' <- occurs env' alts
+            return $ I.Case t' alts'
+          (I.MetaVar mc) -> I.MetaVar <$> occurs env mc
+
+instance Occurs I.MetaClosure where
+  occurs env mc@(I.MetaVarClosure nid nc) = if nid == current env
+    then Env.err [DS "Detected a cycle while occurs-checking", DD nid]
+    else do
+    if (position env) /= Flexible
+    then do
+     _ <- prune env mc
+     let (names, terms) = unzip nc
+     terms' <- mapM (occurs env) terms
+     return $ I.MetaVarClosure nid (zip names terms)
+    else do
+     let (names, terms) = unzip nc
+     terms' <- mapM (occurs env) terms
+     return $ I.MetaVarClosure nid (zip names terms)
+
+
+instance Occurs I.Match where
+  occurs env (I.Match body) = do
+    (pat, body') <- Unbound.unbind body
+    body'' <- occurs (extendLocals env $ pat2Vars pat) body'
+    return $ I.Match (Unbound.bind pat body'')
+
+instance Occurs I.TName where
+  occurs env v =
+    if v `elem` locals env ++ globals env
+    then Env.err [DS "Encountered a variable which isn't allowed", DD v]
+    else return v
+
+instance Occurs I.Arg where
+  occurs env (I.Arg e t) = I.Arg e <$> occurs env t
+
 
 pat2Vars :: I.Pattern -> [I.TName]
 pat2Vars (I.PatVar x) = [x]
 pat2Vars (I.PatCon _ ps) = concatMap (pat2Vars . fst) ps
-
-occursCheckMeta :: (MonadSolver c m)
-                => I.MetaVarId
-                -> [I.TName]
-                -> I.MetaClosure
-                -> m I.MetaClosure
-occursCheckMeta mid tel mc = do
-  -- check if the meta is the same one
-  -- if so, we have a cycle error out
-  let (I.MetaVarClosure nid nc) = mc
-  if nid == mid
-  then Env.err [DS "Detected a cycle while solving", DD mid]
-  else do
-    _ <- prune mc tel
-    -- otherwise, check the meta
-    let (names, terms) = unzip nc
-    terms' <- mapM (occursCheck mid tel) terms
-    return $ I.MetaVarClosure nid (zip names terms)
-
-occursCheckVar :: (MonadSolver c m)
-               => I.MetaVarId
-               -> [I.TName]
-               -> I.TName
-               -> m I.TName
-occursCheckVar mid tel v =
-  if v `elem` tel
-  then return v
-  else  Env.err [DS "Encountered a variable which isn't allowed", DD mid]
-
-occursCheckArgs :: (MonadSolver c m)
-                => I.MetaVarId
-                -> [I.TName]
-                -> [I.Arg]
-                -> m [I.Arg]
-occursCheckArgs mid tel = mapM (occursCheckArg mid tel)
-
-occursCheckArg :: (MonadSolver c m)
-               => I.MetaVarId
-               -> [I.TName]
-               -> I.Arg ->
-               m I.Arg
-occursCheckArg mid tel (I.Arg e t) = I.Arg e <$> occursCheck mid tel t
-
 
 data PruneResult
   = NothingToPrune   -- ^ the kill list is empty or only @False@s
@@ -206,7 +232,7 @@ data PruneResult
     deriving (Eq, Show)
 
 prune :: (MonadSolver c m)
-  => I.MetaClosure  -- ^ Meta to prune.
-  -> [I.TName]  -- ^ disallowed variable.
+  => OccursData
+  -> I.MetaClosure
   -> m PruneResult
-prune (I.MetaVarClosure m cl) tel = undefined
+prune env (I.MetaVarClosure m cl) = undefined
